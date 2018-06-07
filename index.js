6155c00dddbe81f11627f69eae85dc52e6e2415f
Lena Stotska <i.olena.stotska@gmail.com>
2018-06-07 10:38:35 +0300

    feat(line): check line on equal cell

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const container = document.querySelector('.container');
+const gameField = document.querySelector('.game-field');
 let lastType;
 
 function startDragging(e) {
   const target = e.target;
   const type = target.classList.contains('move-x') ? 'x' : 'o';
 
-  if (lastType === type) {
-    if (type === 'x') {
-      alert('Next player - O');
-      return;
-    } else {
-      alert('Next player - X');
-      return;
-    }
-  };
+  if (lastType === type) { return };
 
   const clone = target.cloneNode(true);
 
@@ -30,32 +23,33 @@ function startDragging(e) {
   }
 
   function upDown(e) {
-    const whereIsEl = document.elementsFromPoint(e.pageX, e.pageY);
+    const whereIsEl = document.elementsFromPoint(e.pageX, e.pageY)
 
     if (whereIsEl[1].classList.contains('cell') && whereIsEl[1].children.length === 0) {
       whereIsEl[1].appendChild(clone);
       clone.style.position = '';
-      highlight(whereIsEl[1]);
+      highlight(whereIsEl[1])
     } else {
       clone.parentNode.removeChild(clone);
     }
 
-    document.documentElement.style.userSelect = '';
-    document.removeEventListener('mousemove', moveAt);
-    document.removeEventListener('mouseup', upDown);
+    document.removeEventListener('mousemove', moveAt)
+    document.removeEventListener('mouseup', upDown)
+    gameOver(gameField.children)
   }
 
   document.addEventListener('mousemove', moveAt);
-  document.addEventListener('mouseup', upDown);
+  document.addEventListener('mouseup', upDown)
+
 }
 
 function highlight(cell) {
-  const cellChildren = cell.children;
+  const cellChildren = cell.children
 
   if (cellChildren[0].classList.contains('move-x')) {
-    cell.style.backgroundColor = '#f6f6f6';
+    cell.classList.add('x')
   } else {
-    cell.style.backgroundColor = '#ccc';
+    cell.classList.add('o')
   }
 }
 
@@ -63,3 +57,113 @@ Array.from(container.querySelectorAll('.move-x, .move-o')).forEach((element) => 
   element.addEventListener('mousedown', startDragging);
 });
 
+function gameOver(cells) {
+  const arrCells = Array.from(cells)
+  const resultGame = []
+
+  arrCells.forEach(el => {
+    resultGame.push(el.className.split(' '))
+  })
+
+  return addline(resultGame, cells)
+}
+
+function addline(items, cells) {
+  const result = items.map(el => el.slice(1).join(''))
+
+  if(result[0] === 'x' && result[1] === 'x' && result[2] === 'x') {
+    cells[0].classList.add('finish')
+    cells[1].classList.add('finish')
+    cells[2].classList.add('finish')
+  }
+
+  if(result[0] === 'o' && result[1] === 'o' && result[2] === 'o') {
+    cells[0].classList.add('finish')
+    cells[1].classList.add('finish')
+    cells[2].classList.add('finish')
+  }
+
+  if(result[3] === 'x' && result[4] === 'x' && result[5] === 'x') {
+    cells[3].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[5].classList.add('finish')
+  }
+
+  if(result[3] === 'o' && result[4] === 'o' && result[5] === 'o') {
+    cells[3].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[5].classList.add('finish')
+  }
+
+  if(result[6] === 'x' && result[7] === 'x' && result[8] === 'x') {
+    cells[6].classList.add('finish')
+    cells[7].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[6] === 'o' && result[7] === 'o' && result[8] === 'o') {
+    cells[6].classList.add('finish')
+    cells[7].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[0] === 'x' && result[3] === 'x' && result[6] === 'x') {
+    cells[0].classList.add('finish')
+    cells[3].classList.add('finish')
+    cells[6].classList.add('finish')
+  }
+
+  if(result[0] === 'o' && result[3] === 'o' && result[6] === 'o') {
+    cells[0].classList.add('finish')
+    cells[3].classList.add('finish')
+    cells[6].classList.add('finish')
+  }
+
+  if(result[1] === 'x' && result[4] === 'x' && result[7] === 'x') {
+    cells[1].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[7].classList.add('finish')
+  }
+
+  if(result[1] === 'o' && result[4] === 'o' && result[7] === 'o') {
+    cells[1].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[7].classList.add('finish')
+  }
+
+  if(result[2] === 'x' && result[5] === 'x' && result[8] === 'x') {
+    cells[2].classList.add('finish')
+    cells[5].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[2] === 'o' && result[5] === 'o' && result[8] === 'o') {
+    cells[2].classList.add('finish')
+    cells[5].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[0] === 'x' && result[4] === 'x' && result[8] === 'x') {
+    cells[0].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[0] === 'o' && result[4] === 'o' && result[8] === 'o') {
+    cells[0].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[8].classList.add('finish')
+  }
+
+  if(result[2] === 'x' && result[4] === 'x' && result[6] === 'x') {
+    cells[2].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[6].classList.add('finish')
+  }
+
+  if(result[2] === 'o' && result[4] === 'o' && result[6] === 'o') {
+    cells[2].classList.add('finish')
+    cells[4].classList.add('finish')
+    cells[6].classList.add('finish')
+  }
+}
